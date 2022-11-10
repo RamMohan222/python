2eddbc30d69573b16b098fa8644a4e6c937c6b88
RamMohan <ram222mohan@gmail.com>
2022-11-10 14:39:11 +0530

    Create print_demo.py

---
+++ print_demo.py
@@ -0,0 +1,5 @@
+text = 'RAM'
+
+print(f'{text}:#<20'); #print's text in right #################RAM
+print(f'{text}:#>20'); #print's text in left  RAM#################
+print(f'{text}:.^20'); #print's text in center ........RAM.........
